bca4512376848a69fdbc05ea60dd3cf4914d812c
tilenn <to8803@student.uni-lj.si>
2022-03-09 12:28:52 +0100

    working version

--- izivi/izziv_02/prog.java
+++ izivi/izziv_02/prog.java
@@ -2,6 +2,13 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class prog {
+    public static void zgradiKopico(int[] a) {
+        for (int i = a.length - 1; i >= 0; i--) {
+            pogrezni(a, i, a.length);
+            System.out.println(i + ":" + Arrays.toString(a));
+        }
+    }
+
     public static void pogrezni(int[] a, int i, int dolzKopice) {
         // TODO
         // child nodes are at index (i * 2) + 1 and (i * 2) + 2
@@ -52,9 +59,13 @@ public class prog {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[] arr = { 9, 8, 7, 6, 5, 1, 3, 2, 4 };
-
-        for (int i = 0; i < 9; i++) {
+        int[] arr = { 5, 2, 7, 6, 8, 1, 3, 9, 4 };
+        zgradiKopico(arr);
+        System.out.println(Arrays.toString(arr));
+        System.exit(0);
+        // int[] arr = { 9, 8, 7, 6, 5, 1, 3, 2, 4 };
+        // int[] arr = { 6, 9, 4 };
+        for (int i = 0; i < 2; i++) {
             System.out.println(Arrays.toString(arr));
 
             int tmp = arr[arr.length - i - 1];
@@ -69,7 +80,6 @@ public class prog {
         // for (int i = 0; i < n; i++) {
         // nums[i] = sc.nextInt();
         // }
-
         sc.close();
     }
 }
