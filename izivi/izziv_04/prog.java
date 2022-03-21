379739f3c98f55dd27147140cddf395d39585352
tilenn <to8803@student.uni-lj.si>
2022-03-21 15:04:49 +0100

    izziv4 malo na lepse

--- izivi/izziv_04/prog.java
+++ izivi/izziv_04/prog.java
@@ -4,18 +4,15 @@ import java.util.Scanner;
 public class prog {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int[] stevila = new int[n];
         int[] binStrings = new int[32];
-        HashMap<Integer, Integer> num2numOfOnes = new HashMap<>();
 
         for (int i = 0; i < stevila.length; i++) {
             int readNum = sc.nextInt();
             stevila[i] = readNum;
-            int numOfOnes = (int) Integer.toBinaryString(readNum).chars().filter(ch -> ch == '1').count();
+            int numOfOnes = (int) Integer.bitCount(readNum);
             binStrings[numOfOnes] += 1;
-            num2numOfOnes.put(readNum, numOfOnes);
         }
 
         for (int i = 1; i < binStrings.length; i++) {
@@ -25,7 +22,7 @@ public class prog {
         int[] sortedArr = new int[n];
         for (int i = stevila.length - 1; i >= 0; i--) {
             int numToInsert = stevila[i];
-            int numOfOnes = num2numOfOnes.get(numToInsert);
+            int numOfOnes = Integer.bitCount(numToInsert);
             int position = binStrings[numOfOnes] - 1;
             System.out.printf("(%d,%d)\n", numToInsert, position);
             sortedArr[position] = stevila[i];
